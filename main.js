193777a60b9ac6f4ba075c98779a976f0ce9a7ef
Xer0-PT <joelflm@gmail.com>
2023-01-09 22:51:11 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const repo = "javascript_test";
+const username = "Xer0-PT";
+const token = "";
+
+const filePath = "path/to/my-file.txt";
+
+// Create the API endpoint for reading a file
+const apiUrl = `https://api.github.com/repos/${username}/${repo}/contents/${filePath}`;
+
+// Set the API request options
+const options = {
+  headers: {
+    Authorization: `Token ${token}`,
+  },
+};
+
+// Send the API request
+fetch(apiUrl, options)
+  .then((response) => response.json())
+  .then((data) => {
+    // The file content is returned as base64-encoded data
+    const base64Content = data.content;
+
+    // Decode the base64-encoded data to get the plain text content
+    const fileContent = atob(base64Content);
+
+    console.log(fileContent);
+  });
