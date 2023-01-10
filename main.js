59b76c6540f3e25ac30154bb9ddeba4f9f03f396
Xer0-PT <joelflm@gmail.com>
2023-01-10 00:08:40 +0000

    update

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-require('dotenv').config();
-
-process.env.ACCESS_TOKEN;
-
 const repo = "javascript_test";
 const username = "Xer0-PT";
-const token = ACCESS_TOKEN;
+const token = "YOUR_TOKEN";
 
-const filePath = "path/to/my-file.txt";
+const filePath = "teste.txt";
 
 // Create the API endpoint for reading a file
 const apiUrl = `https://api.github.com/repos/${username}/${repo}/contents/${filePath}`;
+
+var fileContent;
 
 // Set the API request options
 const options = {
@@ -18,15 +16,52 @@ const options = {
   },
 };
 
-// Send the API request
-fetch(apiUrl, options)
-  .then((response) => response.json())
-  .then((data) => {
-    // The file content is returned as base64-encoded data
-    const base64Content = data.content;
+getBananas();
 
-    // Decode the base64-encoded data to get the plain text content
-    const fileContent = atob(base64Content);
+async function getBananas() {
+  await fetch(apiUrl, options)
+    .then((response) => response.json())
+    .then((data) => {
+      // The file content is returned as base64-encoded data
+      const base64Content = data.content;
 
-    console.log(fileContent);
+      // Decode the base64-encoded data to get the plain text content
+      fileContent = atob(base64Content);
+
+      console.log(fileContent);
+    });
+}
+
+const newContent = fileContent + "," + "NOME_DO_BANANA_LIDO_DO_HTML";
+
+addBananas(newContent);
+
+async function addBananas(newContent) {
+  const sha = await getShaFromLastCommit();
+
+  debugger;
+  await fetch(apiUrl, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${token}`,
+    },
+    path: {
+        owner: username,
+        repo: repo,
+        path: filePath,
+    },
+    body: JSON.stringify({
+      message: "Adicionar mais bananas",
+      content: btoa(newContent),
+      sha: sha,
+    }),
   });
+}
+
+async function getShaFromLastCommit() {
+  const response = await fetch(apiUrl);
+  debugger;
+  const data = await response.json();
+  return data.sha;
+}
